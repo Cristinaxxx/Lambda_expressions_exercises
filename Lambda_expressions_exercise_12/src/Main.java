f8cd2eb2e725d73d9f195a8841ed544376489a64
Cristinaxxx <117525933+Cristinaxxx@users.noreply.github.com>
2023-08-23 09:45:02 +0200

    Initial commit

---
+++ Lambda_expressions_exercise_12/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(5);
+        numbers.add(3);
+        numbers.add(9);
+        numbers.add(1);
+
+        int sum = numbers.stream()
+                .mapToInt(n -> n.intValue())
+                .sum();
+
+        System.out.println(sum);
+
+        int multiplication = numbers.stream()
+                .mapToInt(n -> n.intValue())
+                .reduce(1, (x, y) -> (x * y));
+
+        System.out.println(multiplication);
+
+
+    }
+}
